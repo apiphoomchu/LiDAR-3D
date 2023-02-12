4340d388206f2e7d77a7ac28ae712807f6890c9f
Api Phoom <apiphoom23@gmail.com>
2023-02-12 21:02:35 +0700

    update : record LiDAR

--- LiDAR/ViewController.swift
+++ LiDAR/ViewController.swift
@@ -14,18 +14,24 @@ class ViewController: UIViewController, ARSessionDelegate {
     
     var orientation: UIInterfaceOrientation {
         guard let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation else {
-            fatalError("Fatal Error : Can't resolve an issue")
+            fatalError()
         }
         return orientation
     }
     
+    @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
     lazy var imageViewSize: CGSize = {
-        CGSize(width: view.bounds.size.width, height: view.bounds.size.height)
+        CGSize(width: view.bounds.size.width, height: imageViewHeight.constant)
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        arView.session.delegate = self
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recordLidar(_ sender: UIButton) {
+    
         func setARViewOptions(){
             arView.debugOptions.insert(.showSceneUnderstanding)
         }
@@ -38,21 +44,14 @@ class ViewController: UIViewController, ARSessionDelegate {
             if type(of: configuration).supportsFrameSemantics(.sceneDepth) {
                 configuration.frameSemantics = .sceneDepth
             }
-            
             return configuration
-            
         }
         
-        func initARView(){
-            setARViewOptions()
-            let configuration = buildConfigure()
-            arView.session.run(configuration)
-        }
-        
-        arView.session.delegate = self
-        super.viewDidLoad()
-        initARView()
-        
+        setARViewOptions()
+        let configuration = buildConfigure()
+        arView.session.run(configuration)
     }
+    
+    
 }
 
